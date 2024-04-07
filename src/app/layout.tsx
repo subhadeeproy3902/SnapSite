5f7ef3183c0e8db5b872693d1d5a3fb618eea338
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-04-07 21:05:12 +0530

    initial run and lessgo complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "SnapSite v.1.0",
   description: "Take screenshots of websites with ease.",
+  //Opengraph
+  openGraph: {
+    type: "website",
+    images: [
+      {
+        url: "https://i.postimg.cc/63ZfYZwG/529shots-so.webp",
+        width: 1200,
+        height: 630,
+        alt: "SnapSite",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
